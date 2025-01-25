e9f9cc48a57e4269bd8336e3c5953f55b6e76b33
Mexty <m.harchenko@i.ua>
2025-01-25 15:40:53 +0200

    Update ReactMob7.1

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -38,30 +38,21 @@
 
 import Contact from '../Contact/Contact';
 import { useSelector } from 'react-redux';
-// import { selectContacts } from '../../redux/select';
-// import { selectFilter } from '../../redux/select';
+
 import s from './ContactList.module.css';
 import { selectFilteredContacts } from '../../redux/select';
-
-
 
 const ContactList = () => {
 
   const filteredContacts = useSelector(selectFilteredContacts)
-  // const contacts = useSelector(selectContacts);  
-  // const filter = useSelector(selectFilter);  
-  
-  
-  // const filteredContacts = contacts.filter((contact) =>
-  //   filter ? contact.name.toLowerCase().includes(filter.toLowerCase()) : true
-  // );
+ 
 
   
   return (
     <ul>
       {filteredContacts.length > 0 ? (
-        filteredContacts.map(item => (
-          <Contact {...item} key={item.id} />
+        filteredContacts.map((item, index) => (
+          <Contact {...item} key={`${item.id}-${index}`} />
         ))
       ) : (
         <div className={s.box}>
